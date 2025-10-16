29bb628df30b607051d3e10a0ca5eedfc90ada02
Alba Díaz Melián <albadiazgraficaimpresa@gmail.com>
2025-10-16 17:52:37 +0100

    publicación

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -7,13 +7,13 @@ import '../styles/app.css'
 
 const App = () => {
 
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />}/>
-      <Route path="character/:id" element={<DetailPage />}/>
-      <Route path="/*" element={<NotFoundPage />}/>
-    </Routes>
-  )
+    return (
+        <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="character/:id" element={<DetailPage />}/>
+        <Route path="/*" element={<NotFoundPage />}/>
+        </Routes>
+    )
 }
 
 export default App
